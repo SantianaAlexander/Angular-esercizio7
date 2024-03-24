1856b8a6bcb12b7030adede99e6f1eec7ccb4392
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-03-24 20:52:56 +0000

    Vers. 1.0.1

--- esercizio7/src/app/app.routes.ts
+++ esercizio7/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ChikoritaComponent } from './chikorita/chikorita.component';
+import { CyndaquilComponent } from './cyndaquil/cyndaquil.component';
+import { TotodileComponent } from './totodile/totodile.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path:"",
+        component: HomeComponent
+    },
+];
--- esercizio7/src/app/home/home.component.ts
+++ esercizio7/src/app/home/home.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { TotodileComponent } from '../totodile/totodile.component';
+import { CyndaquilComponent } from '../cyndaquil/cyndaquil.component';
+import { ChikoritaComponent } from '../chikorita/chikorita.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TotodileComponent, CyndaquilComponent, ChikoritaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  mostraHome: boolean = true
+  mostraChikorita: boolean = false
+  mostraCyndaquil: boolean = false
+  mostraTotodile: boolean = false
+
   name1 = "chikorita";
   name2 = "cyndaquil";
   name3 = "totodile";
@@ -18,4 +29,31 @@ export class HomeComponent {
   color1 = "#C80408";
   color2 = "#007F86";
   color3 = "#ED8599";
+
+  // CHIKORITA
+  apriChikorita(event: MouseEvent) {
+    this.mostraChikorita = true
+    this.mostraHome = false
+    this.mostraCyndaquil = false
+    this.mostraTotodile = false
+    console.log(this.mostraChikorita)
+  }
+
+  // CYNDAQUIL
+  apriCyndaquil(event: MouseEvent) {
+    this.mostraCyndaquil = true
+    this.mostraHome = false
+    this.mostraChikorita = false
+    this.mostraTotodile = false
+    console.log(this.mostraCyndaquil)
+  }
+
+  // TOTODILE
+  apriTotodile(event: MouseEvent) {
+    this.mostraTotodile = true
+    this.mostraHome = false
+    this.mostraCyndaquil = false
+    this.mostraChikorita = false
+    console.log(this.mostraTotodile)
+  }
 }
