d15dfb0dfae48ed705b677bfb39d946b81909562
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-03-25 18:36:44 +0000

    Vers. Finale

--- esercizio7/src/app/app.routes.ts
+++ esercizio7/src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ChikoritaComponent } from './chikorita/chikorita.component';
-import { CyndaquilComponent } from './cyndaquil/cyndaquil.component';
-import { TotodileComponent } from './totodile/totodile.component';
 
 
 export const routes: Routes = [
--- esercizio7/src/app/home/home.component.ts
+++ esercizio7/src/app/home/home.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
-import { TotodileComponent } from '../totodile/totodile.component';
-import { CyndaquilComponent } from '../cyndaquil/cyndaquil.component';
-import { ChikoritaComponent } from '../chikorita/chikorita.component';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, TotodileComponent, CyndaquilComponent, ChikoritaComponent],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatDividerModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
-  mostraHome: boolean = true
-  mostraChikorita: boolean = false
-  mostraCyndaquil: boolean = false
-  mostraTotodile: boolean = false
+  visualizza: string = "home"
+  constructor() {
+    this.visualizza = "home"
+  }
 
   name1 = "chikorita";
   name2 = "cyndaquil";
@@ -31,29 +29,68 @@ export class HomeComponent {
   color3 = "#ED8599";
 
   // CHIKORITA
+  imageChik: string = "https://media.pokemoncentral.it/wiki/5/53/Artwork0152_Corp_2.png"
+  nomeChik: string = "chikorita"     
+  tipoChik: string = "erba"
+  pesoChik: string = "6,4 kg"
+  altezzaChik: string = "0,9 m"
+  npokedexChik: number = 152
+  descrizioneChik: string = "Chikorita e' un piccolo Pokemon quadrupede dalla pelle di colore verde molto pallida. Ha le zampe tozze con un artiglio piatto su ciascuna zampa e una piccola coda, il collo e' largo quanto la testa con attorno dei boccioli verdi, gli occhi sono grandi con l'iride rossa e sul capo ha la sua caratteristica foglia verde."
+  doveChik: string = "Per ottenere Chikorita bisogna recarsi al laboratorio del Professor Elm nella citta' di New Bark Town. Una volta parlato col professore quest'ultimo chiedera' al giocatore quale pokemon scegliere: Chikorita sara' uno di essi"
+
   apriChikorita(event: MouseEvent) {
-    this.mostraChikorita = true
-    this.mostraHome = false
-    this.mostraCyndaquil = false
-    this.mostraTotodile = false
-    console.log(this.mostraChikorita)
+    this.visualizza = "chikorita"
+    console.log(this.visualizza)
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
   }
 
   // CYNDAQUIL
+  imageCynd: string = "https://media.pokemoncentral.it/wiki/5/5f/Artwork0155_Corp_2.png"
+  nomeCynd: string = "cyndaquil"     
+  tipoCynd: string = "fuoco"
+  pesoCynd: string = "7,9 kg"
+  altezzaCynd: string = "0,5 m"
+  npokedexCynd: number = 155
+  descrizioneCynd: string = "Cyndaquil e' un Pokemon di tipo Fuoco simile a un toporagno con un muso lungo, occhi chiusi e zampe corte. Ha una pelliccia beige con una fronte, un muso superiore e una schiena verde scuro. Quattro macchie rosse sulla sua schiena emettono fiamme."
+  doveCynd: string = "All'inizio del gioco, il Professor Elm ti dara' la possibilita' di scegliere uno dei tre Pokemon iniziali: Cyndaquil, Chikorita o Totodile. Scegliendo Cyndaquil, quest'ultimo sara' il tuo primo compagno di viaggio nella tua avventura."
+
   apriCyndaquil(event: MouseEvent) {
-    this.mostraCyndaquil = true
-    this.mostraHome = false
-    this.mostraChikorita = false
-    this.mostraTotodile = false
-    console.log(this.mostraCyndaquil)
+    this.visualizza = "cyndaquil"
+    console.log(this.visualizza)
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
   }
 
   // TOTODILE
+  imageToto: string = "https://media.pokemoncentral.it/wiki/4/4a/Artwork0158_Corp.png"
+  nomeToto: string = "totodile"     
+  tipoToto: string = "acqua"
+  pesoToto: string = "9,5 kg"
+  altezzaToto: string = "0,6 m"
+  npokedexToto: number = 158
+  descrizioneToto: string = "Totodile e' un Pokemon bipede simile a un coccodrillo di colore azzurro con due strisce gialle a forma di V sul petto. Ha cinque artigli sulle zampe anteriori e tre dita robuste su quelle posteriori. La sua schiena presenta squame appuntite rosse, una anche sulla coda. Ha occhi gialli con segni neri intorno e una bocca larga con grandi zanne."
+  doveToto: string = "All'inizio del tuo viaggio in Pokemon Oro e Argento, il Professor Elm ti offrira' una scelta fondamentale: Cyndaquil, Chikorita o Totodile. Per far si' che Totodile sia il tuo primo compagno d'avventure non bisogna fare altro che sceglierlo."
+
   apriTotodile(event: MouseEvent) {
-    this.mostraTotodile = true
-    this.mostraHome = false
-    this.mostraCyndaquil = false
-    this.mostraChikorita = false
-    console.log(this.mostraTotodile)
+    this.visualizza = "totodile"
+    console.log(this.visualizza)
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
   }
+
+  // TORNA ALLA HOME
+    tornaHome(event: MouseEvent) {
+      this.visualizza = "home"
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
 }
