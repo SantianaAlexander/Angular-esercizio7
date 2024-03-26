c11aa58dc95916517b95bb56a5352c71014381e3
Alexander Santiana <santiana.a@iisgalvanimi.edu.it>
2024-03-26 19:03:14 +0000

    Vers Finito finito

--- esercizio7/src/app/home/home.component.ts
+++ esercizio7/src/app/home/home.component.ts
@@ -35,6 +35,11 @@ export class HomeComponent {
   pesoChik: string = "6,4 kg"
   altezzaChik: string = "0,9 m"
   npokedexChik: number = 152
+  debChik1: string = "fuoco"
+  debChik2: string = "ghiaccio"
+  debChik3: string = "veleno"
+  debChik4: string = "volante"
+  debChik5: string = "coleottero"
   descrizioneChik: string = "Chikorita e' un piccolo Pokemon quadrupede dalla pelle di colore verde molto pallida. Ha le zampe tozze con un artiglio piatto su ciascuna zampa e una piccola coda, il collo e' largo quanto la testa con attorno dei boccioli verdi, gli occhi sono grandi con l'iride rossa e sul capo ha la sua caratteristica foglia verde."
   doveChik: string = "Per ottenere Chikorita bisogna recarsi al laboratorio del Professor Elm nella citta' di New Bark Town. Una volta parlato col professore quest'ultimo chiedera' al giocatore quale pokemon scegliere: Chikorita sara' uno di essi"
 
@@ -54,6 +59,9 @@ export class HomeComponent {
   pesoCynd: string = "7,9 kg"
   altezzaCynd: string = "0,5 m"
   npokedexCynd: number = 155
+  debCynd1: string = "acqua"
+  debCynd2: string = "terra"
+  debCynd3: string = "roccia"
   descrizioneCynd: string = "Cyndaquil e' un Pokemon di tipo Fuoco simile a un toporagno con un muso lungo, occhi chiusi e zampe corte. Ha una pelliccia beige con una fronte, un muso superiore e una schiena verde scuro. Quattro macchie rosse sulla sua schiena emettono fiamme."
   doveCynd: string = "All'inizio del gioco, il Professor Elm ti dara' la possibilita' di scegliere uno dei tre Pokemon iniziali: Cyndaquil, Chikorita o Totodile. Scegliendo Cyndaquil, quest'ultimo sara' il tuo primo compagno di viaggio nella tua avventura."
 
@@ -73,6 +81,8 @@ export class HomeComponent {
   pesoToto: string = "9,5 kg"
   altezzaToto: string = "0,6 m"
   npokedexToto: number = 158
+  debToto1: string = "erba"
+  debToto2: string = "elettro"
   descrizioneToto: string = "Totodile e' un Pokemon bipede simile a un coccodrillo di colore azzurro con due strisce gialle a forma di V sul petto. Ha cinque artigli sulle zampe anteriori e tre dita robuste su quelle posteriori. La sua schiena presenta squame appuntite rosse, una anche sulla coda. Ha occhi gialli con segni neri intorno e una bocca larga con grandi zanne."
   doveToto: string = "All'inizio del tuo viaggio in Pokemon Oro e Argento, il Professor Elm ti offrira' una scelta fondamentale: Cyndaquil, Chikorita o Totodile. Per far si' che Totodile sia il tuo primo compagno d'avventure non bisogna fare altro che sceglierlo."
 
